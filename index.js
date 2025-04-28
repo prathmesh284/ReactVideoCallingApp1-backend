cf4730d58aa42fee75cbe3eade949bfe8cda4e99
Pratham <jadhavprathmesh284@gmail.com>
2025-04-28 23:18:46 +0530

    server

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ app.get('/healthz', (req, res) => {
 });
 
 // --- SERVER START --- //
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, '0.0.0.0',() => {
   console.log(`🌍 Server running on http://0.0.0.0:${PORT}`);
 });
